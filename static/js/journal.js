602b47f74e0ba08b9c91306b3b6370689465a6b6
KKira <albertovega22@gmail.com>
2019-08-14 09:32:42 -0400

    fixed modal issues, ready for merge

--- static/js/journal.js
+++ static/js/journal.js
@@ -25,3 +25,26 @@ function defaultEntry() {
 }
 
 addEventListener("load", defaultEntry);
+
+var modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function(btn){
+	btn.onclick = function() {
+		var modal = btn.getAttribute('data-modal');
+		document.getElementById(modal).style.display = "block";
+		}
+	});
+
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+btn.onclick = function() {
+		var modal = btn.closest('.modal');
+		modal.style.display = "none";
+}
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target.className === "modal") {
+		event.target.style.display = "none";
+	}
+}
